37df38540dfc41176853d2ecbe9b2c10a596786c
Kevin Sawicki <kevinsawicki@gmail.com>
2014-11-19 08:52:24 -0800

    Show all maps under sections

--- index.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.js-tab').on('click', function(event) {
-    $('.nav-tabs').find('.active').removeClass('active');
-    $('.js-map-profit, .js-map-cash, .js-map-eps').addClass('hidden');
-    $('.js-map-' + $(event.currentTarget).data('map')).removeClass('hidden');
-    $(event.currentTarget).addClass('active');
-    return false;
-  });
-});
